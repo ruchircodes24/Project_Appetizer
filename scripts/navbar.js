8f5a647eeebe3fba562118e603181cdd1d752460
ruchircodes24 <gavshinderuchir@gmail.com>
2023-09-25 14:20:28 +0530

    modified:   index.html 	modified:   scripts/navbar.js 	modified:   styles/home.css 	modified:   styles/navbar.css

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,10 +1,23 @@
+let collapsibleButton;
+let collapsibleContent;
 document.addEventListener('DOMContentLoaded', function(){
-  const collapsibleButton = document.querySelector('.menu-button-js');
-  const collapsibleContent = document.querySelector('.collapsible-content-js');
+  collapsibleButton = document.querySelector('.menu-button-js');
+  collapsibleContent = document.querySelector('.collapsible-content-js');
 
   collapsibleButton.addEventListener('click', function(){
     collapsibleContent.classList.toggle('active');
-    collapsibleButton.classList.toggle('active');
-    
+    collapsibleButton.classList.add('active');
   })
 })
+
+document.addEventListener('click', function(event){
+  if (event.target === collapsibleButton && collapsibleContent.style.display === "none") {
+    collapsibleContent.style.display = "flex";
+  } else if (event.target != collapsibleButton && event.target.parentNode != collapsibleButton) {
+    collapsibleContent.classList.remove('active');
+    collapsibleButton.classList.remove('active');
+  }
+})
+
+
+
