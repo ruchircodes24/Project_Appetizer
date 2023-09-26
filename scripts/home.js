59053c4f3798e1f3508fdde420ef74de313c3ef7
ruchircodes24 <gavshinderuchir@gmail.com>
2023-09-26 15:03:30 +0530

    modified:   index.html 	modified:   scripts/home.js 	modified:   styles/home.css 	modified:   styles/navbar.css

--- scripts/home.js
+++ scripts/home.js
@@ -1,12 +1,16 @@
 let cartQuantity = 0;
 let divElement = document.querySelectorAll('.breakfast-item-js, .lunch-item-js, .dinner-item-js, .dessert-item-js, .wine-item-js, .drinks-item-js');
+const viewCart = document.querySelector('.your-cart');
 
 divElement.forEach(element => {
     element.addEventListener('click', function handleClick(event){
-        console.log(`Item Added 
-        cartQuantity = ${cartQuantity = addItem(cartQuantity)} Total Cost: $${cartQuantity * 29}`), event});
+        const itemAdded = document.querySelector('.total-items');
+        const orderTotal = document.querySelector('.total-bill')
+        itemAdded.innerHTML = `Items: ${cartQuantity = addItem(cartQuantity)}`;
+        orderTotal.innerHTML = `$${cartQuantity*29}`;
+        viewCart.classList.add('display-cart')
+        , event});
 });
-
 
 function addItem(cartQuantity){
     return cartQuantity+=1;  
